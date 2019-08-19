73e531d8fb9e5a126895ea9806eef5b47f08f009
miguelorjuela45 <miguel-orjuelar@unilibre.edu.co>
2019-08-19 17:00:02 -0500

    Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  numero1 = null;
+  numero2 = null;
+  resultado = null;
+  operador = '';
+
+  calcular(){
+    if (this.numero1 == null && this.numero2 == null)
+    {
+      return null;
+    }
+    if(this.operador == '+')
+    {
+      this.resultado = this.numero1 + this.numero2;
+    }
+    if(this.operador == '-')
+    {
+      this.resultado = this.numero1 - this.numero2;
+    }
+    if(this.operador == '*')
+    {
+      if (this.numero1 == null || this.numero2 == null)
+      {
+        return null;
+      }
+      this.resultado = this.numero1 * this.numero2;
+    }
+    if(this.operador == '/')
+    {
+      if (this.numero1 == null || this.numero2 == null || this.numero2 == 0)
+      {
+        return null;
+      }
+      this.resultado = this.numero1 / this.numero2;
+    }
+    if(this.operador == '%')
+    {
+      if (this.numero1 == null || this.numero2 == null || this.numero2 == 0)
+      {
+        return null;
+      }
+      this.resultado = this.numero1 % this.numero2;
+    }
+    return this.resultado;
+  }
+  borrar(){
+    this.numero1 = null;
+    this.numero2 = null;
+    this.resultado = null;
+    this.operador = '';
+  }
 }
